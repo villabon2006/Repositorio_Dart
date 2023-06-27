c770838cf98369c4ccc244555bd8ce7cb43612e4
miguel_villabon <miguelangelvillabonquintero@gmail.com>
2023-06-27 16:29:11 -0500

    subiendo ejercicio 4

--- Ejercicios-Dart/ejercicio3.dart
+++ Ejercicios-Dart/ejercicio3.dart
@@ -1,4 +1,5 @@
 void main() {
+  //resta de numeros decimales "double"
   double n1 = 3.4;
   double n2 = 4.1;
   double resta = n1 - n2;
---
+++ Ejercicios-Dart/ejercicio4.dart
@@ -0,0 +1,10 @@
+void main() {
+  /*ejercicio: un aprendiz del SENA recibe un subsidio base, 
+  recibe una comision del 10% de total de monitorias
+  en investigacion SENNOVA se desea saber cuanto ganara al mes.*/
+
+  double sub = 1000000;
+  double mon = sub * 0.10;
+  double total = mon + sub;
+  print("el valor total recibido por el aprendiz ed de $total");
+}
